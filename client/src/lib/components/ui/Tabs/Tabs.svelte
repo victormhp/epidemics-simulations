abcca6ae52d89405150ad03e8f42e7b4eddf4c2d
victormhp <victoralbertomhp@gmail.com>
2023-10-01 22:24:32 -0600

    Improve Tabs component

--- client/src/lib/components/ui/Tabs/Tabs.svelte
+++ client/src/lib/components/ui/Tabs/Tabs.svelte
@@ -2,24 +2,70 @@
 	import { setSelectedTab, getSelectedTab, setTabTitles } from '$lib/stores/tabs.store';
 
 	let selectedTab = '1';
+	let tabList: HTMLElement;
 
 	setSelectedTab(selectedTab);
 	$: selectedTabStore = getSelectedTab();
 
 	const titles = setTabTitles([]);
+
+	function onKeyDown(event: KeyboardEvent) {
+		if (event.code === 'ArrowRight') {
+			const tabs = Array.from(tabList.querySelectorAll('.tab'));
+
+			const currTab = event.currentTarget as HTMLElement;
+			const currIndex = tabs.indexOf(currTab);
+
+			const nextIndex = currIndex + 1 >= tabs.length ? 0 : currIndex + 1;
+			const nextTab = tabs[nextIndex] as HTMLButtonElement;
+
+			if (nextTab) {
+				nextTab.click();
+				nextTab.focus();
+			}
+		} else if (event.code === 'ArrowLeft') {
+			const tabs = Array.from(tabList.querySelectorAll('.tab'));
+
+			const currTab = event.currentTarget as HTMLElement;
+			const currIndex = tabs.indexOf(currTab);
+
+			const nextIndex = currIndex - 1 < 0 ? tabs.length - 1 : currIndex - 1;
+			const nextTab = tabs[nextIndex] as HTMLButtonElement;
+
+			if (nextTab) {
+				nextTab.click();
+				nextTab.focus();
+			}
+		}
+	}
 </script>
 
-<div class="flex justify-between">
-	{#each $titles as { id, title }}
+<div bind:this={tabList} class="tab-list flex justify-between" role="tablist">
+	{#each $titles as { id, title } (id)}
 		<button
-			class="btn-tab {$selectedTabStore === id ? 'border-foreground' : null}"
-			on:click={() => {
-				$selectedTabStore = id;
-			}}
+			class="tab btn-tab"
+			class:selected={$selectedTabStore === id}
+			role="tab"
+			tabindex={$selectedTabStore === id ? 0 : -1}
+			aria-selected={$selectedTabStore === id}
+			on:click={() => ($selectedTabStore = id)}
+			on:keydown={onKeyDown}
 		>
 			{title}
 		</button>
 	{/each}
 </div>
 
-<slot {selectedTab} />
+<div
+	class="rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-4"
+	role="tabpanel"
+	tabindex="0"
+>
+	<slot {selectedTab} />
+</div>
+
+<style lang="postcss">
+	.selected {
+		@apply border-foreground z-50;
+	}
+</style>
