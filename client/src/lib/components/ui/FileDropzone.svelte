03590f6fd00d6799b87ecb61b2715321414441c2
victormhp <victoralbertomhp@gmail.com>
2023-10-01 22:26:11 -0600

    Add format size file utility function

--- client/src/lib/components/ui/FileDropzone.svelte
+++ client/src/lib/components/ui/FileDropzone.svelte
@@ -1,38 +1,41 @@
 <script lang="ts">
+	import { formatFileSize } from '$lib/utils';
 	import { Upload } from 'lucide-svelte';
 
+	export let id: string;
 	export let allowedExtensions: string;
 	export let files: FileList | undefined = undefined;
 </script>
 
 <div class="flex flex-col items-center justify-center w-full">
 	<label
-		for="dropzone-file"
+		for="dropzone-{id}"
 		class="flex flex-col items-center justify-center w-full h-64 border-2 border-border border-dashed rounded-lg cursor-pointer bg-muted hover:bg-muted/50 transition-colors"
 	>
 		<div class="flex flex-col items-center justify-center pt-5 pb-6">
 			<Upload size="32" color="#71717a" />
-			<p class="my-2 text-sm text-muted-foreground">
+			<p class="my-2 text-xs text-muted-foreground sm:text-sm">
 				<span class="font-semibold">Click to upload</span> or drag and drop
 			</p>
 			<p class="text-xs text-muted-foreground">
 				{#if files && files[0]}
 					{#each Array.from(files) as file}
-						{file.name} ({file.size} bytes)
+						{file.name} ({formatFileSize(file.size)})
 					{/each}
 				{:else}
 					<slot name="files" />
 				{/if}
 			</p>
 		</div>
-		<input
-			id="dropzone-file"
-			name="file-chart"
-			bind:files
-			type="file"
-			accept={allowedExtensions}
-			class="hidden"
-			required
-		/>
+		<div class="h-0 w-0 overflow-hidden">
+			<input
+				bind:files
+				id="dropzone-{id}"
+				name="fileChart"
+				type="file"
+				accept={allowedExtensions}
+				required
+			/>
+		</div>
 	</label>
 </div>
--- client/src/lib/utils/common.utils.ts
+++ client/src/lib/utils/common.utils.ts
@@ -27,3 +27,13 @@ export function formJsonToYaml(data: ChartInputs): string {
 		.map(([key, value]) => `${key}: ${value}`)
 		.join('\n');
 }
+
+export function formatFileSize(bytes: number) {
+	if (bytes == 0) return '0 Bytes';
+
+	const k = 1000,
+		sizes = ['Bytes', 'KB', 'MB', 'GB'],
+		i = Math.floor(Math.log(bytes) / Math.log(k));
+
+	return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
