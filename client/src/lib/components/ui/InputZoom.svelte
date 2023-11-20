94a374323bbeb7d9e2ed269114948a414c44e0a8
victormhp <victoralbertomhp@gmail.com>
2023-11-20 13:04:22 -0600

    Add input zoom component

---
+++ client/src/lib/components/ui/InputZoom.svelte
@@ -0,0 +1,11 @@
+<div class="flex">
+	<div class="flex items-center h-5">
+		<input class="scale-110" id="zoom" name="zoom" type="checkbox" aria-describedby="enable-zoom" />
+	</div>
+	<div class="ml-2 text-sm">
+		<label for="zoom" class="font-medium text-primary">Enable zoom and pan</label>
+		<p class="text-xs text-primary/50 select-none">
+			To enable zoom and pan functionality, you will need to use a chart with smoothed lines.
+		</p>
+	</div>
+</div>
