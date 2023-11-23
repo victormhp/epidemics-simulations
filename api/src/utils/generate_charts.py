4257a516475c4688d1a61f1ab3f2b2480f37a92a
victormhp <victoralbertomhp@gmail.com>
2023-11-23 11:05:48 -0600

    Typed functions and dicts

--- api/src/utils/generate_charts.py
+++ api/src/utils/generate_charts.py
@@ -2,12 +2,17 @@ import EoN
 import networkx as nx
 from EoN import Simulation_Investigation
 from simplification.cutil import simplify_coords
-from typing import Type, List
+from numpy.typing import NDArray
+from typing import Any, Callable
 
-GraphType = Type[nx.Graph]
+
+model_simulations: dict[str, Callable[..., tuple | Simulation_Investigation]] = {
+    "sir": EoN.fast_SIR,
+    "sis": EoN.fast_SIS,
+}
 
 
-model_strategies = {
+model_strategies: dict[str, dict[str, Callable[..., tuple]]] = {
     "sir": {
         "Homogeneous pairwise": EoN.SIR_homogeneous_pairwise_from_graph,
         "Heterogeneous meanfield": EoN.SIR_heterogeneous_meanfield_from_graph,
@@ -21,47 +26,10 @@ model_strategies = {
     },
 }
 
-model_simulations = {
-    "sir": EoN.fast_SIR,
-    "sis": EoN.fast_SIS,
-}
 
-
-def run_strategy(model: str, strategy: str, label: str, G: GraphType, tau: float, gamma: float, rho: float, tmax: float):
-    strategies = []
-
-    if model == "sir":
-        t, S, I, R = strategy(G, tau, gamma, rho=rho, tmax=tmax)
-    else:
-        t, S, I = strategy(G, tau, gamma, rho=rho, tmax=tmax)
-
-    points = [[ti, Ii] for ti, Ii in zip(t.tolist(), I.tolist())]
-    strategies.extend([{"strategy": label, "x": x, "y": y} for [x, y] in points])
-
-    return strategies
-
-
-def run_simulations(model: str, states: List[str], iterations: int, G: GraphType, tau: float, gamma: float, rho: float, tmax: float, zoom: bool):
-    simulations = []
-    model = model_simulations[model]
-
-    for i in range(iterations):
-        sim_object: Simulation_Investigation = model(G, tau, gamma, rho=rho, tmax=tmax, return_full_data=True)
-        t, D = sim_object.summary()
-
-        for key, values in D.items():
-            if key in states:
-                points = [[ti, ki] for ti, ki in zip(t.tolist(), values.tolist())]
-            
-                if zoom:
-                    points = simplify_coords(points, 0.05)
-            
-                simulations.extend([{"strategy": f"Simulation {key} - {i + 1}", "x": x, "y": y} for [x, y] in points])
-
-    return simulations
-
-
-def get_model_data(G: GraphType, model: str, states: List[str], tau: float, gamma: float, rho: float, zoom: bool):
+def get_model_data(
+    G: type[nx.Graph], model: str, states: list[str], tau: float, gamma: float, rho: float, zoom: bool
+) -> list[dict[str, str | float]]:
     tmax = 20
     iterations = 5  # run 5 simulations
 
@@ -79,11 +47,11 @@ def get_model_data(G: GraphType, model: str, states: List[str], tau: float, gamm
     return data
 
 
-def get_model_data_from_sim(t, D, zoom: bool):
+def get_model_data_from_sim(t: NDArray, D: dict[Any, list[int]], zoom: bool) -> list[dict[str, str | float]]:
     data = []
 
     for key, values in D.items():
-        points = [[ti, ki] for ti, ki in zip(t.tolist(), values.tolist())]
+        points = [[ti, ki] for ti, ki in zip(t.tolist(), values)]
 
         if zoom:
             points = simplify_coords(points, 0.05)
@@ -91,3 +59,60 @@ def get_model_data_from_sim(t, D, zoom: bool):
         data.extend([{"strategy": f"Simulation: {key}", "x": x, "y": y} for [x, y] in points])
 
     return data
+
+
+def run_simulations(
+    model: str,
+    states: list[str],
+    iterations: int,
+    G: type[nx.Graph],
+    tau: float,
+    gamma: float,
+    rho: float,
+    tmax: float,
+    zoom: bool,
+) -> list[dict[str, str | float]]:
+    simulations = []
+    model_function = model_simulations[model]
+
+    for i in range(iterations):
+        sim_object = model_function(G, tau, gamma, rho=rho, tmax=tmax, return_full_data=True)
+
+        if isinstance(sim_object, Simulation_Investigation):
+            t, D = sim_object.summary()
+
+            for key, values in D.items():
+                if key in states:
+                    points = [[ti, ki] for ti, ki in zip(t.tolist(), values)]
+
+                    if zoom:
+                        points = simplify_coords(points, 0.05)
+
+                    simulations.extend(
+                        [{"strategy": f"Simulation {key} - {i + 1}", "x": x, "y": y} for [x, y] in points]
+                    )
+
+    return simulations
+
+
+def run_strategy(
+    model: str,
+    strategy_function: Callable[..., tuple],
+    label: str,
+    G: type[nx.Graph],
+    tau: float,
+    gamma: float,
+    rho: float,
+    tmax: float,
+) -> list[dict[str, str | float]]:
+    strategies = []
+
+    if model == "sir":
+        t, S, I, R = strategy_function(G, tau, gamma, rho=rho, tmax=tmax)
+    else:
+        t, S, I = strategy_function(G, tau, gamma, rho=rho, tmax=tmax)
+
+    points = [[ti, Ii] for ti, Ii in zip(t.tolist(), I.tolist())]
+    strategies.extend([{"strategy": label, "x": x, "y": y} for [x, y] in points])
+
+    return strategies
