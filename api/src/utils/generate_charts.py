e318c44295c3c8792ad081a170715e9c30ab0a7f
victormhp <victoralbertomhp@gmail.com>
2023-12-01 20:42:56 -0600

    Merge generate and run chart functions

--- api/src/utils/generate_charts.py
+++ api/src/utils/generate_charts.py
@@ -1,86 +1,22 @@
-import EoN
-import networkx as nx
 from EoN import Simulation_Investigation
 from simplification.cutil import simplify_coords
-from numpy.typing import NDArray
-from typing import Any, Callable
+from src.config.algorithms import EpidemicAlgorithm
 
 
-model_strategies: dict[str, dict[str, Callable[..., tuple]]] = {
-    "sir": {
-        "Homogeneous pairwise": EoN.SIR_homogeneous_pairwise_from_graph,
-        "Heterogeneous meanfield": EoN.SIR_heterogeneous_meanfield_from_graph,
-        "EBCM approximation": EoN.EBCM_from_graph,
-        "Prefix mix EBCM": EoN.EBCM_pref_mix_from_graph,
-    },
-    "sis": {
-        "Homogeneous pairwise": EoN.SIS_homogeneous_pairwise_from_graph,
-        "Heterogeneous meanfield": EoN.SIS_heterogeneous_meanfield_from_graph,
-        "Compact Pairwise": EoN.SIS_compact_pairwise_from_graph,
-    },
-}
-
-
-def get_model_data(G, model_function, zoom: bool, states: list[str], tau: float, gamma: float, rho: float) -> list[dict[str, str | float]]:
-    tmax = 20
-    iterations = 5  # run 5 simulations
-
-    data = []
-
-    # Add simulations
-    simulations = run_simulations(
-        G,
-        model_function,
-        zoom,
-        states,
-        iterations,
-        tau,
-        gamma,
-        rho,
-        tmax,
-    )
-
-    data.extend(simulations)
-
-    # Add strategies
-    # strategies = model_strategies[model]
-    # for label, strategy in strategies.items():
-    #     data.extend(run_strategy(model, strategy, label, G, tau, gamma, rho, tmax))
-
-    return data
-
-
-def get_model_data_from_sim(t: NDArray, D: dict[Any, NDArray], zoom: bool) -> list[dict[str, str | float]]:
-    data = []
-
-    for key, values in D.items():
-        points = [[ti, ki] for ti, ki in zip(t.tolist(), values.tolist())]
-
-        if zoom:
-            points = simplify_coords(points, 0.05)
-
-        data.extend([{"strategy": f"Simulation: {key}", "x": x, "y": y} for [x, y] in points])
-
-    return data
-
-
-def run_simulations(
+def get_model_data(
     G,
-    model_function,
-    zoom: bool,
+    model_function: EpidemicAlgorithm,
     states: list[str],
     iterations: int,
-    tau: float,
-    gamma: float,
-    rho: float,
-    tmax: float,
+    zoom: bool,
+    **kwargs,
 ) -> list[dict[str, str | float]]:
-    simulations = []
+    data = []
 
     for i in range(iterations):
-        sim_object = model_function(G, tau, gamma, rho=rho, tmax=tmax, return_full_data=True)
+        sim = model_function(G, **kwargs, return_full_data=True)
 
-        t, D = sim_object.summary()
+        t, D = sim.summary()
 
         for key, values in D.items():
             if key in states:
@@ -89,29 +25,22 @@ def run_simulations(
                 if zoom:
                     points = simplify_coords(points, 0.05)
 
-                simulations.extend([{"strategy": f"Simulation {key} - {i + 1}", "x": x, "y": y} for [x, y] in points])
+                data.extend([{"strategy": f"Simulation {key} - {i + 1}", "x": x, "y": y} for [x, y] in points])
 
-    return simulations
+    return data
 
 
-def run_strategy(
-    model: str,
-    strategy_function: Callable[..., tuple],
-    label: str,
-    G: type[nx.Graph],
-    tau: float,
-    gamma: float,
-    rho: float,
-    tmax: float,
-) -> list[dict[str, str | float]]:
-    strategies = []
+def get_model_data_from_sim(sim: Simulation_Investigation, states: list[str], zoom: bool) -> list[dict[str, str | float]]:
+    data = []
+    t, D = sim.summary()
 
-    if model == "sir":
-        t, S, I, R = strategy_function(G, tau, gamma, rho=rho, tmax=tmax)
-    else:
-        t, S, I = strategy_function(G, tau, gamma, rho=rho, tmax=tmax)
+    for key, values in D.items():
+        if key in states:
+            points = [[ti, ki] for ti, ki in zip(t.tolist(), values.tolist())]
 
-    points = [[ti, Ii] for ti, Ii in zip(t.tolist(), I.tolist())]
-    strategies.extend([{"strategy": label, "x": x, "y": y} for [x, y] in points])
+            if zoom:
+                points = simplify_coords(points, 0.05)
 
-    return strategies
+            data.extend([{"strategy": f"Simulation {key}", "x": x, "y": y} for [x, y] in points])
+
+    return data
