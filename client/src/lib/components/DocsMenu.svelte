238773f62a396051bed3037e401caa89b193fff3
victormhp <victoralbertomhp@gmail.com>
2023-12-11 21:46:31 -0600

    Add DocsMenu

---
+++ client/src/lib/components/DocsMenu.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { docsMenu } from '$lib/config';
+	import { getHash } from '$lib/utils';
+	import { page } from '$app/stores';
+</script>
+
+<aside
+	class="fixed w-[min(25rem,30vw)] h-full left-0 bg-muted/50 overflow-x-hidden overflow-y-auto hidden border-r-2 border-muted lg:block"
+>
+	<nav class="h-[calc(100vh-4rem)] overflow-x-hidden overflow-y-auto">
+		<ul class="space-y-12 text-muted-foreground ml-auto px-0 pl-28 py-28">
+			{#each docsMenu.sections as { title, href, items }}
+				<li class="space-y-2">
+					<a {href} class="font-semibold text-foreground tracking-wider uppercase">
+						{title}
+					</a>
+					<ul class="space-y-2 border-l border-muted-foreground">
+						{#each items as { title, href }}
+							<li>
+								<a
+									{href}
+									class="border-l border-muted-foreground pl-4 -ml-px hover:text-foreground hover:border-foreground"
+									class:active={$page.url.hash === getHash(href)}
+								>
+									{title}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</aside>
+
+<style>
+	.active {
+		@apply text-foreground border-foreground;
+	}
+</style>
--- client/src/lib/components/index.ts
+++ client/src/lib/components/index.ts
@@ -2,3 +2,4 @@ export { default as ChartForm } from './ChartForm.svelte';
 export { default as ChartFormYaml } from './ChartFormYaml.svelte';
 export { default as ChartFormSim } from './ChartFormSim.svelte';
 export { default as EpidemicsChart } from './EpidemicsChart.svelte';
+export { default as DocsMenu } from './DocsMenu.svelte';
