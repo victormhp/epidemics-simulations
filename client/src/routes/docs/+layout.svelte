f85f9768bee6881b21580065256d65cedbe8d805
victormhp <victoralbertomhp@gmail.com>
2023-12-13 15:30:45 -0600

    Delete unnecessary api for docs

--- client/src/routes/docs/+layout.svelte
+++ client/src/routes/docs/+layout.svelte
@@ -1,17 +1,10 @@
 <script lang="ts">
-	import type { LayoutData } from './$types';
 	import { DocsMenu } from '$lib/components';
-
-	export let data: LayoutData;
-
-	$: pagePath = data.pathname;
 </script>
 
-{#key pagePath}
-	<div class="flex">
-		<main class="max-w-[125ch] px-4 py-28 lg:pl-[min(25rem,30vw)]">
-			<slot />
-		</main>
-		<DocsMenu />
-	</div>
-{/key}
+<div class="flex">
+	<main class="max-w-[125ch] px-4 py-28 lg:pl-[min(25rem,30vw)]">
+		<slot />
+	</main>
+	<DocsMenu />
+</div>
--- client/src/routes/docs/+layout.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutLoad } from './$types';
-
-export const load: LayoutLoad = async ({ fetch, url }) => {
-	const response = await fetch('/api/docs');
-	const docs = await response.json();
-	const pathname = url.pathname;
-
-	return { docs, pathname };
-};
