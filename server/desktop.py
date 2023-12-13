9ae3ea6321b224264d9272df2d4f98b165643ab0
victormhp <victoralbertomhp@gmail.com>
2023-12-13 15:30:14 -0600

    Update desktop

--- server/desktop.py
+++ server/desktop.py
@@ -1,6 +1,10 @@
-from index import app
+from src import init_app
 from flaskwebgui import FlaskUI
 
-if __name__ == '__main__':
-    FlaskUI(app=app, server="flask").run()
 
+app = init_app()
+ui = FlaskUI(app=app, server="flask")
+
+
+if __name__ == '__main__':
+    ui.run()
