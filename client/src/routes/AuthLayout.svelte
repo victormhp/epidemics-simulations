dc098a7410f0d77928730daf9f6092aa04eec4a5
victormhp <victoralbertomhp@gmail.com>
2024-01-17 17:42:23 -0600

    Remove  unnecessary layout

--- client/src/routes/AuthLayout.svelte
+++
@@ -1,8 +0,0 @@
-<div class="min-h-screen flex">
-	<header class="fixed w-full h-fit py-2 px-8 bg-background border-b border-border shadow-sm z-50">
-		<nav class="flex justify-between items-center">
-			<a href="/" class="text-xl font-bold p-2">Epidemic Simulations</a>
-		</nav>
-	</header>
-	<slot />
-</div>
--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,4 +1,3 @@
-export { default as AuthLayout } from './AuthLayout.svelte';
 export { default as Layout } from './Layout.svelte';
 export { default as Dashboard } from './Dashboard.svelte';
 export { default as Docs } from './Docs.svelte';
